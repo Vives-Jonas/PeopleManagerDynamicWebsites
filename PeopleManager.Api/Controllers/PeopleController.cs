89661b7c55f993fadfcb21682090a9d2eb516d1a
JonasVanluchene <jonas.vanluchene@gmail.com>
2025-09-29 11:23:20 +0200

    Add Web Api

---
+++ PeopleManager.Api/Controllers/PeopleController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PeopleManager.Model;
+using PeopleManager.Services;
+
+namespace PeopleManager.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PeopleController(PersonService personService) : ControllerBase
+    {
+        //FIND
+        [HttpGet]
+        public IActionResult Find()
+        {
+            var people = personService.Find();
+            return Ok(people);
+        }
+
+
+        //GET
+        [HttpGet("{id:int}")]
+        public IActionResult Get([FromRoute]int id)
+        {
+            var people = personService.Get(id);
+            return Ok(people);
+        }
+
+
+        //CREATE
+        [HttpPost]
+        public IActionResult Create([FromBody] Person person)
+        {
+            var newPerson = personService.Create(person);
+            return Ok(newPerson);
+        }
+
+
+        //UPDATE
+        [HttpPut("{id:int}")]
+        public IActionResult Update([FromRoute] int id, [FromBody] Person person)
+        {
+            var updatedPerson = personService.Update(id, person);
+            return Ok(updatedPerson);
+        }
+
+
+        //DELETE
+        [HttpDelete("{id:int}")]
+        public IActionResult Delete([FromRoute] int id)
+        {
+            personService.Delete(id);
+            return Ok();
+        }
+    }
+}
